5254673bd89800c8bfcd5a3f390d62578cf089bb
jasmac <jaskarannagra@hotmail.com>
2022-08-20 16:17:16 -0700

    express tutorial, setting up mongoose and mongodb

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+const mongoose = require('mongoose');
+const mongoDB = 'mongodb+srv://m001-student:m001-mongodb-basics@sandbox.ojhigy8.mongodb.net/?retryWrites=true&w=majority'
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
